bef956dd217c6515236da9a1fa0d8dae6dcb5100
LeenaKH123 <engleena@yahoo.com>
2023-01-15 09:26:15 +0300

    divide the data into training and testing

--- CreditCard2_2/CreditCard.r
+++ CreditCard2_2/CreditCard.r
@@ -6,9 +6,13 @@
 # print(dataset)
 # credit_card_data
 # credit_card_data-headers
-install.packages("pacman")
+#install.packages("pacman")
 pacman::p_load(kernlab, kknn)
 # df <- read.csv('./credit_card_data-headers.txt') -- not working
 df = read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data-headers.txt', header = TRUE) 
 print(df)
 str(df)
+summary(df)
+library(kernlab)
+c.train = df[(1:490), ]
+c.test = df[(1:164), ]
