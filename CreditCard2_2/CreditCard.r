7a196d8a7daa620b2d3e01060cfd90a6bf6df6fa
LeenaKH123 <engleena@yahoo.com>
2023-01-15 09:49:26 +0300

    select the dataset columns

--- CreditCard2_2/CreditCard.r
+++ CreditCard2_2/CreditCard.r
@@ -9,10 +9,16 @@
 #install.packages("pacman")
 pacman::p_load(kernlab, kknn)
 # df <- read.csv('./credit_card_data-headers.txt') -- not working
+# step 1: import the dataset
 df = read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data-headers.txt', header = TRUE) 
+# step 2: select the dataset columns
+df <- df[2:4]
 print(df)
-str(df)
-summary(df)
-library(kernlab)
-c.train = df[(1:490), ]
-c.test = df[(1:164), ]
+# str(df)
+# summary(df)
+# library(kernlab)
+# c.train = df[(1:490), ]
+# c.test = df[(1:164), ]
+# model = ksvm(data=c.train,
+            #  strength = A1+A2,
+            #  kernel="vanilladot") 
