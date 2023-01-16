3a406083ee0863aa4ca41621494ec8740da37c9c
LeenaKH123 <engleena@yahoo.com>
2023-01-16 09:04:48 +0300

    reading the data

--- CreditCard2_2/CreditCard.r
+++ CreditCard2_2/CreditCard.r
@@ -6,14 +6,29 @@
 # print(dataset)
 # credit_card_data
 # credit_card_data-headers
-#install.packages("pacman")
+# install.packages("pacman")
 pacman::p_load(kernlab, kknn)
+
 # df <- read.csv('./credit_card_data-headers.txt') -- not working
 # step 1: import the dataset
 df = read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data-headers.txt', header = TRUE) 
+# call ksvm.  Vanilladot is a simple linear kernel.
+model <- ksvm(df[,1:10],df[,11],type="C-svc",kernel="vanilladot",C=100,scaled=TRUE)
+# calculate a1…am
+# a <- colSums(model@xmatrix[[1]] * model@coef[[1]])
+# a
+# # calculate a0
+# a0 <- –model@b
+# a0
+# # see what the model predicts
+# pred <- predict(model,data[,1:10])
+# pred
+# # see what fraction of the model’s predictions match the actual classification
+# sum(pred == data[,11]) / nrow(data)
+
 # step 2: select the dataset columns
-df <- df[2:4]
-print(df)
+# df <- df[2:4]
+# print(df)
 # str(df)
 # summary(df)
 # library(kernlab)
