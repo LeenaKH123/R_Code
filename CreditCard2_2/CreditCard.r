61ea421347fd71a0848119ab9efaee9dca5fc1a6
LeenaKH123 <engleena@yahoo.com>
2023-01-16 09:49:37 +0300

    accuracy calculated

--- CreditCard2_2/CreditCard.r
+++ CreditCard2_2/CreditCard.r
@@ -8,14 +8,21 @@ CardData <- read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data-headers.
 CardModel <- ksvm(x=as.matrix(CardData[,1:10]),y=as.factor(CardData[,11]),scaled=T,type="C-svc",kernel="vanilladot",C=100)
 # calculate a1…am
 a <- colSums(CardModel@xmatrix[[1]] * CardModel@coef[[1]])
+#printing the value of a
 cat("a = ", a)
 a0 <- CardModel@b
+cat("\n")
+# printing the value of a0
 cat("The value of a0 = ", a0)
+cat("\n")
 # see what the model predicts
 pred <- predict(CardModel,CardData[,1:10])
 # pred
 # # see what fraction of the model’s predictions match the actual classification
-# sum(pred == data[,11]) / nrow(data)
+validateaccuracy = sum(pred == CardData[,11]) / nrow(CardData)
+# print accuracy
+cat("Accuracy =", validateaccuracy)
+cat("\n")
 
 # step 2: select the dataset columns
 # df <- df[2:4]
