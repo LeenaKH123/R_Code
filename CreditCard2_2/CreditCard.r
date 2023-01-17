64152ed9ce99f3a1454f3dcb7288b203796b5e9d
LeenaKH123 <engleena@yahoo.com>
2023-01-17 13:14:39 +0300

    kernel vanilla

--- CreditCard2_2/CreditCard.r
+++ CreditCard2_2/CreditCard.r
@@ -1,6 +1,5 @@
 # install.packages("pacman")
 pacman::p_load(kernlab, kknn)
-# df <- read.csv('./credit_card_data-headers.txt') -- not working
 # step 1: import the dataset
 CardData <- read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data-headers.txt', header = TRUE) 
 # print(CardData) -- I have the date
@@ -12,16 +11,20 @@ a <- colSums(CardModel@xmatrix[[1]] * CardModel@coef[[1]])
 cat("a = ", a)
 a0 <- CardModel@b
 cat("\n")
-# printing the value of a0
+# # printing the value of a0
 cat("The value of a0 = ", a0)
 cat("\n")
-# see what the model predicts
+# formulate the equation to find y
+# y = -0.001*A1
+#see what the model predicts
 pred <- predict(CardModel,CardData[,1:10])
-# pred
+# print prediction
+# cat("Prediction =", pred)
+# cat("\n")
 # # see what fraction of the model’s predictions match the actual classification
 validateaccuracy <- sum(pred == CardData[,11]) / nrow(CardData)
 # print accuracy
-cat("Accuracy =", validateaccuracy)
+cat("Accuracy for c value 100 =", validateaccuracy)
 cat("\n")
 # define a range for c values
 cvalues <- as.list(c(10^(-5:5)))
@@ -33,4 +36,6 @@ for (cvaluescounter in cvalues){
     CardModel <- ksvm(x=as.matrix(CardData[,1:10]),y=as.factor(CardData[,11]),scaled=T,type="C-svc",kernel="vanilladot",C=cvaluescounter)
     pred <- predict(CardModel,CardData[,1:10])
     validateaccuracy <- sum(pred == CardData[,11]) / nrow(CardData)
+    cat("Accuracy is: ", validateaccuracy)
+    cat(" for C value = ",cvaluescounter )
     }
