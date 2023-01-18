87b07ac977c8baab3f1c60c4ad4599d8a408ab03
LeenaKH123 <engleena@yahoo.com>
2023-01-18 13:17:44 +0300

    working KKNN model

--- CreditCard2_2/CreditCard_KKNN.r
+++ CreditCard2_2/CreditCard_KKNN.r
@@ -1,21 +1,23 @@
 # install.packages("kknn")
+rm(list = ls())
 library(kknn)
 # step 1: import the dataset
-CardData <- read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data-headers.txt', header = TRUE) 
+CardData <- read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data.txt', header = FALSE) 
 # ****** KKNN ********
-head(CardData)
-# print(head(CardData)) --> A1, A2, A3, A8, A9, A10, A11, A12, A14, A15, R1
-# calculate the accuracy of the model with a function that pass the value of k
-check_accuracy <- function(M){
-  predicted <- rep(0,(nrow(CardData))) 
-  for (i in 1:nrow(CardData)){
-    model=kknn(R1~A1+A2+A3+A8+A9+A10+A11+A12+A14+A15,CardData[-i,],CardData[i,],k=M, scale = TRUE)
-    predicted[i] <- as.integer(fitted(model)+0.5)
-  }
-  accuracy <- sum(predicted == CardData[,11]) / nrow(data)
- return(accuracy)
-}
-acc <- rep(0,20) # set up a vector of 20 zeros to start
-for (M in 1:20){
-   acc[M] = check_accuracy(M) # test knn with X neighbors
+# head(CardData)
+# print(head(CardData))
+ # calculate the accuracy of the model with a function that pass the value of k
+ check_accuracy <- function(M){
+   predicted <- rep(0,(nrow(CardData)))
+   for (i in 1:nrow(CardData)){
+      model=kknn(V11~V1+V2+V3+V4+V5+V6+V7+V8+V9+V10,CardData[-i,],CardData[i,],k=M, scale = TRUE) # use scaled data
+     predicted[i] <- as.integer(fitted(model) + 0.5)
+   }
+   accuracy <- sum(predicted == CardData[,11]) / nrow(CardData)
+  return(accuracy)
  }
+
+ acc <- rep(0,20) # set up a vector of 20 zeros to start
+ for (M in 2:20){
+    print(check_accuracy(M)) # test knn with X neighbors
+  }
