958cb0e7869a1090259555372f3d47e9411f73e1
LeenaKH123 <engleena@yahoo.com>
2023-01-18 13:25:53 +0300

    done

--- CreditCard2_2/CreditCard_KKNN.r
+++ CreditCard2_2/CreditCard_KKNN.r
@@ -2,7 +2,7 @@
 rm(list = ls())
 library(kknn)
 # step 1: import the dataset
-CardData <- read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data.txt', header = FALSE) 
+CardData <- read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data-headers.txt', header = TRUE) 
 # ****** KKNN ********
 # head(CardData)
 # print(head(CardData))
@@ -10,7 +10,7 @@ CardData <- read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data.txt', he
  check_accuracy <- function(M){
    predicted <- rep(0,(nrow(CardData)))
    for (i in 1:nrow(CardData)){
-      model=kknn(V11~V1+V2+V3+V4+V5+V6+V7+V8+V9+V10,CardData[-i,],CardData[i,],k=M, scale = TRUE) # use scaled data
+      model=kknn(R1~A1+A2+A3+A8+A9+A10+A11+A12+A14+A15,CardData[-i,],CardData[i,],k=M, scale = TRUE) # use scaled data
      predicted[i] <- as.integer(fitted(model) + 0.5)
    }
    accuracy <- sum(predicted == CardData[,11]) / nrow(CardData)
@@ -19,5 +19,6 @@ CardData <- read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data.txt', he
 
  acc <- rep(0,20) # set up a vector of 20 zeros to start
  for (M in 2:20){
-    print(check_accuracy(M)) # test knn with X neighbors
+   acc[M] <- (check_accuracy(M)) # test knn with X neighbors
   }
+  print(acc)
