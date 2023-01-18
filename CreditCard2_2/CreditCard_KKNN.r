0ca19f8480534a6a94f3f6f8146f452e614aa557
LeenaKH123 <engleena@yahoo.com>
2023-01-18 13:58:23 +0300

    done done

--- CreditCard2_2/CreditCard_KKNN.r
+++ CreditCard2_2/CreditCard_KKNN.r
@@ -7,7 +7,7 @@ CardData <- read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data-headers.
 # head(CardData)
 # print(head(CardData))
  # calculate the accuracy of the model with a function that pass the value of k
- check_accuracy <- function(M){
+ accuracyfunction <- function(M){
    predicted <- rep(0,(nrow(CardData)))
    for (i in 1:nrow(CardData)){
       modelKKNN=kknn(R1~A1+A2+A3+A8+A9+A10+A11+A12+A14+A15,CardData[-i,],CardData[i,],k=M, scale = TRUE) # use scaled data
@@ -17,8 +17,8 @@ CardData <- read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data-headers.
   return(accuracyequation)
  }
 
- accvector <- rep(0,20) # set up a vector of 20 zeros to start
- for (M in 2:20){
-   accvector[M] <- (check_accuracy(M)) # test knn with X neighbors
+ accvector <- rep(0, 20) # initialise a vector
+ for (M in 1:20){
+   accvector[M] <- (accuracyfunction(M)) # test knn with X neighbors
   }
   print(accvector)
