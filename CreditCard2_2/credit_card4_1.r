fc55039327bda0b81e97b81757e241859f2b8d10
LeenaKH123 <engleena@yahoo.com>
2023-01-22 16:39:26 +0300

    in progress

---
+++ CreditCard2_2/credit_card4_1.r
@@ -0,0 +1,2 @@
+# Describe a situation or problem from your job, 
+# everyday life, current events, etc., for which a clustering model would be appropriate. List some (up to 5) predictors that you might use.
