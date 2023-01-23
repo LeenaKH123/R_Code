3fcf4b305688e9f8cee08ac8e585883ec980eb10
LeenaKH123 <engleena@yahoo.com>
2023-01-23 12:20:08 +0300

    clustering

--- CreditCard2_2/CreditCard3_1.r
+++ CreditCard2_2/CreditCard3_1.r
@@ -2,3 +2,9 @@
 # find a good classifier:
 # using cross-validation (do this for the k-nearest-neighbors model; SVM is optional); and
 # splitting the data into training, validation, and test data sets (pick either KNN or SVM; the other is optional).
+# # install.packages("pacman")
+pacman::p_load(kernlab, kknn)
+# step 1: import the dataset
+rm(list = ls())
+set.seed(123)
+CardData <- read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data-headers.txt', header = TRUE) 
