ea5845c4a93403ba19686c77c29b0d60618463de
LeenaKH123 <engleena@yahoo.com>
2023-01-23 12:36:13 +0300

    training data

--- CreditCard2_2/CreditCard3_1.r
+++ CreditCard2_2/CreditCard3_1.r
@@ -10,3 +10,26 @@ set.seed(123)
 CardData <- read.table('/home/lilo/rcode/CreditCard2_2/credit_card_data-headers.txt', header = TRUE) 
 # head(CardData, 5)
 # print(head(CardData, 5))
+# Splitting the data into training, test and validation
+# 70% for training and 15% for test and another 15% for validation
+
+# 70% training data
+training_data <- sample(nrow(CardData), size = floor(nrow(CardData) * 0.7))
+
+# creating a vector with the training data
+creditcard_training = creditcard_data[mask_trainingset,] 
+
+# Using the remaining data for test and validation sets by splitting into half
+# selecting all rows except rows assigned to training set
+remaining_data = creditcard_data[-mask_trainingset,]  
+
+# Half of remaining data is for validation set and half for test set
+mask_validationset = sample(nrow(remaining_data), size = floor(nrow(remaining_data)/2))
+
+# validation data set
+creditcard_validation = remaining_data[mask_validationset,] 
+
+# test data set
+creditcard_test = remaining_data[-mask_validationset,] 
+
+# Now, validation and test set have 131 observations, which is 20% of total 654 observations
