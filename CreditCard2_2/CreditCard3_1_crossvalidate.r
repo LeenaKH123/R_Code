f320da76d911d3acaba07a3c0edb74fa0b99476e
LeenaKH123 <engleena@yahoo.com>
2023-01-24 20:33:02 +0300

    cross validation in progress

--- CreditCard2_2/CreditCard3_1_crossvalidate.r
+++ CreditCard2_2/CreditCard3_1_crossvalidate.r
@@ -18,22 +18,22 @@ creditcard_test_validation <- CardData[-training_data,]
 # print(creditcard_test_validation) # printing the remaining data in a matrix
 
 model1 <- train.kknn(as.factor(V11)~V1+V2+V3+V4+V5+V6+V7+V8+V9+V10, data = creditcard_training, kmax = 70, scale = TRUE)
-# print(model1)
+print(model1)
 # minimial misclassification: 0.1444201
 # best kernel: optimal
 # best k = 10
 # create a vector of zeros with the size of the training data matrix
-PredictWithTrainingData <- rep(0,(nrow(creditcard_training)))
-# initialise a variable with zero to use it in summing the predictions later
-TrainPredictionSum <- 0
+# PredictWithTrainingData <- rep(0,(nrow(creditcard_training)))
+# # initialise a variable with zero to use it in summing the predictions later
+# TrainPredictionSum <- 0
 
-for (i in 1:nrow(creditcard_training)){
-  model2=kknn(V11~V1+V2+V3+V4+V5+V6+V7+V8+V9+V10,creditcard_training[-i,],creditcard_training[i,],k = 10,kernel="optimal", scale = TRUE) 
-  PredictWithTrainingData[i]<- as.integer(fitted(model2)+0.5) # round off to 0 or 1 and store predicted values in vector
-}
+# for (i in 1:nrow(creditcard_training)){
+#   model2=kknn(V11~V1+V2+V3+V4+V5+V6+V7+V8+V9+V10,creditcard_training[-i,],creditcard_training[i,],k = 10,kernel="optimal", scale = TRUE) 
+#   PredictWithTrainingData[i]<- as.integer(fitted(model2)+0.5) # round off to 0 or 1 and store predicted values in vector
+# }
 
-# calculate fraction of correct predictions to find accuracy
-train_accuracy<- sum(PredictWithTrainingData == creditcard_training[,11]) / nrow(creditcard_training)
-print(train_accuracy)
-#-----------------------------------------------
-# training data to calculate accuracy with 
+# # calculate fraction of correct predictions to find accuracy
+# train_accuracy<- sum(PredictWithTrainingData == creditcard_training[,11]) / nrow(creditcard_training)
+# print(train_accuracy)
+# #-----------------------------------------------
+# # training data to calculate accuracy with 
