a248ac8421d5f919a57ed336b0cf1bdbdc55d2c8
LeenaKH123 <engleena@yahoo.com>
2023-01-25 12:48:02 +0300

    flower data in progress

--- CreditCard2_2/credit_card_4_2_iris.r
+++ CreditCard2_2/credit_card_4_2_iris.r
@@ -17,14 +17,25 @@ set.seed(123)
 # data <- read.table('/home/lilo/rcode/CreditCard2_2/iris.txt', header = TRUE)
 # table(data$species)
 # flowerdata <- data[training_data,]
-library(ggplot2)
 # ggplot(data, aes(sepal_length, sepal_width, color = species)) + geom_point()
-
 # cluster_1 <- kmeans(data[, 1:4], centers = 2, nstart = 5)
 # cluster_2 <- kmeans(data[, 1:4], centers = 3, nstart = 10)
 # cluster_1$tot.withinss
 # table(cluster_1$cluster, data$Species)
-df <- iris
-ggplot(df, aes(Petal.Length, Petal.Width)) + geom_point(aes(col=Species), size=4)
-set.seed(101)
-irisCluster <- kmeans(df[,1:4], center=3, nstart=20)
+library(ggplot2)
+flowerdata <- iris
+p <- ggplot(flowerdata, aes(Sepal.Length, Sepal.Width, color = Species)) + geom_point()
+print(p)
+cluster_1 <- kmeans(flowerdata[,1:4], center = 2, nstart = 5)
+cluster_2 <- kmeans(flowerdata[,1:4], center = 3, nstart = 10)
+cluster_3 <- kmeans(flowerdata[,1:4], center = 5, nstart = 15)
+
+# table(irisCluster$cluster, df$Species)
+# library(cluster)
+# # clusplot(iris, irisCluster$cluster, color=T, shade=T, labels=0, lines=0)
+# tot.withinss <- vector(mode="character", length=10)
+# for (i in 1:10){
+#   irisCluster <- kmeans(df[,1:4], center=i, nstart=20)
+#   tot.withinss[i] <- irisCluster$tot.withinss
+# }
+# plot(1:10, tot.withinss, type="b", pch=19)
