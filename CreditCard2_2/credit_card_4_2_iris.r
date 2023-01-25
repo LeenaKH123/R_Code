fbf9fc2e9688d40d57ad6174f203a2cc324e2063
LeenaKH123 <engleena@yahoo.com>
2023-01-25 13:15:50 +0300

    iris cluster completed

--- CreditCard2_2/credit_card_4_2_iris.r
+++ CreditCard2_2/credit_card_4_2_iris.r
@@ -15,23 +15,25 @@ set.seed(123)
 library(ggplot2)
 flowerdata <- iris
 p <- ggplot(flowerdata, aes(Sepal.Length, Sepal.Width, color = Species)) + geom_point()
-# print(p)
-cluster_1 <- kmeans(flowerdata[,1:4], center = 2, nstart = 5)
-cluster_2 <- kmeans(flowerdata[,1:4], center = 3, nstart = 20)
-cluster_3 <- kmeans(flowerdata[,1:4], center = 5, nstart = 15)
-table1 <- table(cluster_1$cluster, flowerdata <- iris$Species)
-print(table1)
-table2 <- table(cluster_2$cluster, flowerdata <- iris$Species)
-print(table2)
-table3 <- table(cluster_3$cluster, flowerdata <- iris$Species)
-print(table3)
-library(cluster)
+# # print(p)
+# cluster_1 <- kmeans(flowerdata[,1:4], center = 2, nstart = 5)
+# cluster_2 <- kmeans(flowerdata[,1:4], center = 3, nstart = 20)
+# cluster_3 <- kmeans(flowerdata[,1:4], center = 5, nstart = 15)
+# table1 <- table(cluster_1$cluster, flowerdata <- iris$Species)
+# print(table1)
+# table2 <- table(cluster_2$cluster, flowerdata <- iris$Species)
+# print(table2)
+# table3 <- table(cluster_3$cluster, flowerdata <- iris$Species)
+# print(table3)
+# library(cluster)
 # cluster1plot <- clusplot(iris, cluster_1$cluster, color=T, shade=T, labels=0, lines=0)
-cluster2plot <- clusplot(iris, cluster_2$cluster, color=T, shade=T, labels=0, lines=0)
+# cluster2plot <- clusplot(iris, cluster_2$cluster, color=T, shade=T, labels=0, lines=0)
 # cluster3plot <- clusplot(iris, cluster_3$cluster, color=T, shade=T, labels=0, lines=0)
-# tot.withinss <- vector(mode="character", length=10)
-# for (i in 1:10){
-#   irisCluster <- kmeans(df[,1:4], center=i, nstart=20)
-#   tot.withinss[i] <- irisCluster$tot.withinss
-# }
-# plot(1:10, tot.withinss, type="b", pch=19)
+
+#  ** total within cluster sum of squares **
+tot.withinss <- vector(mode="character", length=10)
+for (i in 1:10){
+  irisCluster <- kmeans(flowerdata[,1:4], center=i, nstart=20)
+  tot.withinss[i] <- irisCluster$tot.withinss
+}
+plot(1:10, tot.withinss, type="b", pch=19)
