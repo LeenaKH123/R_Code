1549027676a9a9ebd246021a995eee7413c1c35e
LeenaKH123 <engleena@yahoo.com>
2023-01-29 13:22:56 +0300

    outliers test

--- uscrime/outliers.r
+++ uscrime/outliers.r
@@ -1,2 +1,3 @@
 crimeDate = read.table("/home/lilo/rcode/uscrime/uscrime.txt", header= TRUE, stringsAsFactors = F) 
 head(crimeDate)
+grubbs.test(crimeDate[,16], type = 11, opposite = FALSE, two.sided = FALSE )
