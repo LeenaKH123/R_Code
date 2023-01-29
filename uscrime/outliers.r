52026d0a4982341f7d6f6a1be67b35b2faae6524
LeenaKH123 <engleena@yahoo.com>
2023-01-29 16:10:45 +0300

    experimenting with grub

--- uscrime/outliers.r
+++ uscrime/outliers.r
@@ -1,5 +1,13 @@
 # install.packages('outliers')
 library(outliers)
 crimeDate = read.table("/home/lilo/rcode/uscrime/uscrime.txt", header= TRUE, stringsAsFactors = F) 
-head(crimeDate)
-grubbs.test(crimeDate[,16], type = 11, opposite = FALSE, two.sided = FALSE)
+# head(crimeDate)
+# hist(crimeDate[,16], breaks = 5)
+# which.max(crimeDate$Crime)
+# which.min(crimeDate$Crime)
+# summary(crimeDate$Crime)
+# grubbs.test(crimeDate$Crime, type = 10, opposite = FALSE, two.sided = FALSE)
+# grubbs.test(crimeDate$Crime, type = 11, opposite = FALSE, two.sided = FALSE)
+grubbs.result <- grubbs.test(crimeDate$Crime)
+pvalue <- grubbs.result$p.value
+grubbs.result
