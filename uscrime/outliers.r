d40ce414c522f51dd609b5fa15be441c53e556db
LeenaKH123 <engleena@yahoo.com>
2023-01-29 14:05:00 +0300

    outliers done

--- uscrime/outliers.r
+++ uscrime/outliers.r
@@ -1,3 +1,5 @@
+# install.packages('outliers')
+library(outliers)
 crimeDate = read.table("/home/lilo/rcode/uscrime/uscrime.txt", header= TRUE, stringsAsFactors = F) 
 head(crimeDate)
-grubbs.test(crimeDate[,16], type = 11, opposite = FALSE, two.sided = FALSE )
+grubbs.test(crimeDate[,16], type = 11, opposite = FALSE, two.sided = FALSE)
