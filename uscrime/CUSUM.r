fdc9c5f482a2fc7d1a6a8ab65b69d7d05143461b
LeenaKH123 <engleena@yahoo.com>
2023-01-30 20:38:21 +0300

    median

--- uscrime/CUSUM.r
+++ uscrime/CUSUM.r
@@ -1,5 +1,9 @@
 tempData <- read.table("/home/lilo/rcode/uscrime/temprature.txt", header= TRUE, stringsAsFactors = F)
 head(tempData)
 summary(tempData)
-install.packages("reshape")
+# install.packages("reshape")
 library(reshape)
+# use the melt function to transform a wide format into a long format
+q3 <- melt(tempData[], id='DAY')
+print(q3)
+boxplot(data=q3, value~variable)
