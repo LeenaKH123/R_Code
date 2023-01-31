d5dbabd77d96671f99093572001c6666932bf036
LeenaKH123 <engleena@yahoo.com>
2023-01-31 08:48:16 +0300

    cusum in progress

--- uscrime/CUSUM.r
+++ uscrime/CUSUM.r
@@ -5,5 +5,14 @@ summary(tempData)
 library(reshape)
 # use the melt function to transform a wide format into a long format
 q3 <- melt(tempData[], id='DAY')
-print(q3)
-boxplot(data=q3, value~variable)
+# print(q3)
+# boxplot(data=q3, value~variable)
+# ------ day stats
+# 123 is the number of days we have in the source data, so creating a vector of size 123
+day_mean <- rep(0,123)
+for (i in 1:123) {
+  day_mean[i] <- mean(unlist(tempData[i, 2:21]))
+}
+df_mean <- data.frame(day = unlist(tempData[0:123,1]), day.mean)
+print(df_mean)
+# plot(x = 1:123, df_mean$day.mean,col="purple")
