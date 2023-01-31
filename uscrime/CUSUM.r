b12f746cc8e490bdd7c287af8175606b47f90493
LeenaKH123 <engleena@yahoo.com>
2023-01-31 15:19:51 +0300

    almost done with CUSUM

--- uscrime/CUSUM.r
+++ uscrime/CUSUM.r
@@ -17,7 +17,7 @@ df_mean <- data.frame(day = unlist(tempData[0:123,1]), day_mean)
 # print(df_mean)
 # plot(x = 1:123, df_mean$day.mean,col="purple")
 q4 <- melt(tempData[0:20,])
-print(q4)
+# print(q4)
 tempratureMean <- aggregate(value~variable, q4, mean)
 # print(tempratureMean)
 tempratureStandardDev <- aggregate(value~variable, q4, sd)
@@ -27,4 +27,26 @@ tempratureMedian <- aggregate(value~variable, q4, median)
 yearlyStatistics <- data.frame(YEAR = tempratureMean$variable, Median = tempratureMedian$value, MEAN = tempratureMedian$value, SD = tempratureStandardDev$value)
 # print(yearlyStatistics)
 # plot(x = 1996:2015, tempratureMean$value,col="blue", main = "Average temprature of the first 62 days")
-plot(x = 1996:2015, tempratureMedian$value,col="red", main = "Median temprature of the first 62 days")
+# plot(x = 1996:2015, tempratureMedian$value,col="red", main = "Median temprature of the first 62 days")
+# install.packages("qcc")
+library(qcc)
+cusum_matrix <- matrix(0, nrow=ncol(tempData) - 1, ncol=3)
+# print(cusum_matrix)
+for (i in 1:(ncol(tempData)-1))
+{CUSUM_MODEL <- cusum(tempData[,i+1], 
+ center = mean(tempratureMean$value), 
+ std.dev = mean(tempratureStandardDev$value), 
+ decision.interval= 3 * mean(tempratureStandardDev$value),
+ se.shift = 0.5 * mean(tempratureStandardDev$value), plot = T, add.stats = T)
+   # Index for lower violations
+  # This is when temperature starts to cool off
+  v.index <- CUSUM_MODEL$violations$lower
+  model.cusum.violations[i,] <- c(YEAR = colnames(tempData)[i+1],
+                                  Start = tempData[min(v.index),1],
+                                  MedianS = tempData[median(v.index),1])
+}
+print(model.cusum.violations[,1:2])
+plot(as.Date(model.cusum.violations[,2],"%d-%B"), x = 1996:2015,
+     main = "First day when weather starts cooling off",
+     xlab = "Years",
+     ylab = "Dates")
