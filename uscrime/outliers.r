c6d80c40bbc1194f352936cc36ca2359be003ef9
LeenaKH123 <engleena@yahoo.com>
2023-02-06 08:57:17 +0300

    expo smooth

--- uscrime/outliers.r
+++ uscrime/outliers.r
@@ -1,14 +1,15 @@
 # install.packages('outliers')
 library(outliers)
 crimeDate = read.table("/home/lilo/rcode/uscrime/uscrime.txt", header= TRUE, stringsAsFactors = F) 
-# head(crimeDate)
-# hist(crimeDate$Crime, breaks = 20)
-# which.max(crimeDate$Crime)
-# which.min(crimeDate$Crime)
-# summary(crimeDate$Crime)
-# grubbs.test(crimeDate$Crime, type = 10, opposite = FALSE, two.sided = FALSE)
+head(crimeDate)
+hist(crimeDate$Crime, breaks = 20)
+which.max(crimeDate$Crime)
+which.min(crimeDate$Crime)
+summary(crimeDate$Crime)
+grubbs.test(crimeDate$Crime, type = 10, opposite = FALSE, two.sided = FALSE)
 # grubbs.test(crimeDate$Crime, type = 11, opposite = FALSE, two.sided = FALSE)
 grubbs.result <- grubbs.test(crimeDate$Crime)
 pvalue <- grubbs.result$p.value
 grubbs.result
 plot(crimeDate$Crime)
+
