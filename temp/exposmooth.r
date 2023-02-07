b08fe19c06b0e4351000fc6be4faf51c33039ef3
LeenaKH123 <engleena@yahoo.com>
2023-02-07 20:53:45 +0300

    time series created and plotted

--- temp/exposmooth.r
+++ temp/exposmooth.r
@@ -1,11 +1,18 @@
 tempData <- read.table("/home/lilo/rcode/temp/temprature.txt", header= TRUE, stringsAsFactors = F)
-head(tempData)
-summary(tempData)
+timeseries<-ts(as.vector(unlist(tempData[,2:21])),start=1996,frequency=123)
+plot(timeseries)
+
+
+
+
+
+# head(tempData)
+# summary(tempData)
 # install.packages("reshape")
-library(reshape)
-q3 <- melt(tempData[], id='DAY')
-df <- data.frame(q3)
-write.csv(df, "/home/lilo/rcode/temp/reshaped.csv")
+# library(reshape)
+# q3 <- melt(tempData[], id='DAY')
+# df <- data.frame(q3)
+# write.csv(df, "/home/lilo/rcode/temp/reshaped.csv")
 # print(q3)
 # boxplot(data=q3, value~variable)
 # timeseries <- ts(q3, start=1996, frequency = 2460)
