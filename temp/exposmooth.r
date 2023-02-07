93f4794feae01147c993060e8c10fd08c53ee324
LeenaKH123 <engleena@yahoo.com>
2023-02-07 20:58:22 +0300

    found smoothing parameters

--- temp/exposmooth.r
+++ temp/exposmooth.r
@@ -1,9 +1,10 @@
 tempData <- read.table("/home/lilo/rcode/temp/temprature.txt", header= TRUE, stringsAsFactors = F)
 timeseries<-ts(as.vector(unlist(tempData[,2:21])),start=1996,frequency=123)
 plot(timeseries)
-
-
-
+components_dfts <- decompose(timeseries)
+plot(components_dfts)
+HW1 <- HoltWinters(timeseries)
+print(HW1)
 
 
 # head(tempData)
