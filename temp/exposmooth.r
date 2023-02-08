57efce3e9eef4d1d71c435688ea015914950c0d4
LeenaKH123 <engleena@yahoo.com>
2023-02-08 08:57:08 +0300

    holt winter in progress

--- temp/exposmooth.r
+++ temp/exposmooth.r
@@ -1,15 +1,36 @@
 tempData <- read.table("/home/lilo/rcode/temp/temprature.txt", header= TRUE, stringsAsFactors = F)
 timeseries<-ts(as.vector(unlist(tempData[,2:21])),start=1996,frequency=123)
+print(timeseries)
 plot(timeseries)
 components_dfts <- decompose(timeseries)
 library("ggplot2") 
 # plot(components_dfts)
-HW1 <- HoltWinters(timeseries)
+set.seed(1)
+HW1 <- HoltWinters(timeseries,seasonal="additive")
 print(HW1)
-HW2 <- HoltWinters(timeseries, alpha=0.1, beta=0.1, gamma=0.6248076)
-plot(timeseries, ylab="Temprature", col="purple")
+
+set.seed(1)
+HW2 <- HoltWinters(timeseries, alpha=0.2, beta=0.1, gamma=0.1)
+
+plot(timeseries, ylab="Temprature", col="black")
 lines(HW1$fitted[,1], lty=2, col="#00ffaa")
+
+plot(timeseries, ylab="Temprature", col="black")
 lines(HW2$fitted[,1], lty=2, col="red")
+
+set.seed(1)
+HW3 <- HoltWinters(timeseries,seasonal="multiplicative")
+print(HW3)
+plot(timeseries, ylab="Temprature", col="black")
+lines(HW3$fitted[,1], lty=2, col="#00ffaa")
+
+par(mfrow=c(1,2))
+plot(fitted(HW1))
+plot(fitted(HW3))
+
+
+
+
 
 # head(tempData)
 # summary(tempData)
