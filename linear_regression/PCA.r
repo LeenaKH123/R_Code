ebafded116581324ff4f1d723bc944bfc4c506a9
LeenaKH123 <engleena@yahoo.com>
2023-02-22 20:52:27 +0300

    test

--- linear_regression/PCA.r
+++ linear_regression/PCA.r
@@ -8,6 +8,10 @@ mu <- c(10, 5)
 df <- as.data.frame(mvrnorm(n=1000, mu=mu, Sigma = sigma))
 # plot our unscaled data
 ggplot(df, aes(x= V1, y=V2)) + geom_point() + coord_fixed() + ggtitle("The unscaled original data")
+# correlation coefficient of our data
+cor(df)
+# scale our data, center the data where we want it
+data_scaled <- as.data.frame(scale(df))
 # crimeDate = read.table("/home/lilo/rcode/uscrime/uscrime.txt", header= TRUE, stringsAsFactors = F) 
 # head(crimeDate)
 # # crime_model <- lm(Crime ~ ., data = crimeData)
