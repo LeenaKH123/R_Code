ed6926c96c9dc4119842a7a962df6797f9935d0b
LeenaKH123 <engleena@yahoo.com>
2023-02-22 20:25:39 +0300

    code in progress

--- linear_regression/PCA.r
+++ linear_regression/PCA.r
@@ -1,4 +1,4 @@
-crimeDate = read.table("/home/lilo/rcode/uscrime/uscrime.txt", header= TRUE, stringsAsFactors = F) 
-head(crimeDate)
-# crime_model <- lm(Crime ~ ., data = crimeData)
-set.seed(1000)
+# crimeDate = read.table("/home/lilo/rcode/uscrime/uscrime.txt", header= TRUE, stringsAsFactors = F) 
+# head(crimeDate)
+# # crime_model <- lm(Crime ~ ., data = crimeData)
+# set.seed(1000)
