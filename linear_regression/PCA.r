295e2757dd88aa9e8ecb4c30ea78cd0c6c60e32b
LeenaKH123 <engleena@yahoo.com>
2023-02-22 20:32:39 +0300

    sigma is done

--- linear_regression/PCA.r
+++ linear_regression/PCA.r
@@ -1,4 +1,9 @@
+library(MASS)
+library(ggplot2)
+set.seed(1000)
+# define the mean and the standard deviation of random data
+sigma <- rbind(c(1, -0.7), c(-0.7, 1) )
 # crimeDate = read.table("/home/lilo/rcode/uscrime/uscrime.txt", header= TRUE, stringsAsFactors = F) 
 # head(crimeDate)
 # # crime_model <- lm(Crime ~ ., data = crimeData)
-# set.seed(1000)
+# 
