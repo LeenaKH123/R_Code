5c56f221f9ed29517a9f221887b750ed7b582ea1
LeenaKH123 <engleena@yahoo.com>
2023-02-22 20:47:48 +0300

    done plotting the data

--- linear_regression/PCA.r
+++ linear_regression/PCA.r
@@ -5,7 +5,9 @@ set.seed(1000)
 sigma <- rbind(c(1, -0.7), c(-0.7, 1))
 mu <- c(10, 5)
 # randomly generate some multivariate data
-df <- as.data.frame(mvrnorm(n=1000, mu=mu, Sigma = sigma ))
+df <- as.data.frame(mvrnorm(n=1000, mu=mu, Sigma = sigma))
+# plot our unscaled data
+ggplot(df, aes(x= V1, y=V2)) + geom_point() + coord_fixed() + ggtitle("The unscaled original data")
 # crimeDate = read.table("/home/lilo/rcode/uscrime/uscrime.txt", header= TRUE, stringsAsFactors = F) 
 # head(crimeDate)
 # # crime_model <- lm(Crime ~ ., data = crimeData)
