2d8d0b16584f8bb2e2c32e811ae200c489932109
LeenaKH123 <engleena@yahoo.com>
2023-02-27 21:25:16 +0300

    built the regression model

--- advanced_regression/advanced_regression.r
+++ advanced_regression/advanced_regression.r
@@ -1,6 +1,13 @@
+library(tree)
 library(tidyverse)
 library(ggplot2)
 library(randomForest)
-install.packages("tree")
-# library(tree)
+# install.packages("tree")
+# 
 # library(caret)
+crimeDate = read.table("/home/lilo/rcode/uscrime/uscrime.txt", header= TRUE, stringsAsFactors = F)
+head(crimeDate)
+set.seed(1000)
+# build the regression model
+regression_tree <- tree(Crime~., data = crimeDate)
+summary(regression_tree)
