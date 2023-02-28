7b88e8b8fe4aa1262c57ea16b04d40ddd8ccf5e4
LeenaKH123 <engleena@yahoo.com>
2023-02-28 12:23:17 +0300

    created the german data model

--- advanced_regression/advanced_reg2.r
+++ advanced_regression/advanced_reg2.r
@@ -5,3 +5,11 @@ print(head(germanData))
 #make the response variable binary in terms of 0 and 1
 germanData$V21[germanData$V21==1]<-0
 germanData$V21[germanData$V21==2]<-1
+print(head(germanData))
+#split the data into training and testing sets
+germanData_train <- germanData[1:800,]
+germanData_test <- germanData[801:1000,]
+
+# create a logistic regression model
+germanData_model <- glm(V21~.,family = binomial(link = "logit"),data = germanData_train)
+print(germanData_model)
