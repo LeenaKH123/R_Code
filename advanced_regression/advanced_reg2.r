e4fb2106e6a6c0969acf5b0e281f20db5ddf1c9d
LeenaKH123 <engleena@yahoo.com>
2023-02-28 12:14:52 +0300

    done replacing values

--- advanced_regression/advanced_reg2.r
+++ advanced_regression/advanced_reg2.r
@@ -1,2 +1,7 @@
-germanData = read.table("/home/lilo/rcode/advanced_regression/germancredit.txt", header= TRUE, stringsAsFactors = F)
+rm(list = ls())
+germanData = read.table("/home/lilo/rcode/advanced_regression/germancredit.txt", sep = " ")
 print(head(germanData))
+#notice that we have many categorical predictors
+#make the response variable binary in terms of 0 and 1
+germanData$V21[germanData$V21==1]<-0
+germanData$V21[germanData$V21==2]<-1
