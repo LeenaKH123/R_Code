d29bc9c2ad5abd030dcd0eec1db933a283088482
LeenaKH123 <engleena@yahoo.com>
2023-03-08 19:04:52 +0300

    step forward and backward

--- advanced_regression/stepwise.r
+++ advanced_regression/stepwise.r
@@ -6,3 +6,6 @@ step(model_back, direction = "backward")
 step(model_back, direction = "backward", trace = 0)
 #perform forward selection
 model_forward <- lm(Crime~., data =uscrime)
+step(model_forward, scope = formula(lm(Crime~.,data = uscrime)), direction = "forward")
+#perform stepwise regression
+
