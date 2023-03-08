2db8f0910409815a36a2a27e34e985924a77149d
LeenaKH123 <engleena@yahoo.com>
2023-03-08 18:19:20 +0300

    model forward

--- advanced_regression/stepwise.r
+++ advanced_regression/stepwise.r
@@ -1,6 +1,8 @@
 rm(list = ls())
-uscrime -< read.table()
+uscrime <- read.table("/home/lilo/rcode/uscrime/uscrime.txt", header= TRUE, stringsAsFactors = F)
 # perform backward elimination, can use the trace argument to suppress the output
 model_back <- lm(Crime~., data = uscrime)
 step(model_back, direction = "backward")
 step(model_back, direction = "backward", trace = 0)
+#perform forward selection
+model_forward <- lm(Crime~., data =uscrime)
