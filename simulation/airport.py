ff77811c7de5e8f1ff6acada82e8d790e66a3349
LeenaKH123 <engleena@yahoo.com>
2023-03-14 14:50:54 +0300

    simulation almost complete

--- simulation/airport.py
+++ simulation/airport.py
@@ -12,48 +12,32 @@ class Airport(object):
     def check_in(self, passenger):
         yield self.env.timeout(random.poisson())
     def check_ticket(self, passenger):
-        yield self.env.timeout(random.poisson())
-
- 
+        yield self.env.timeout(random.poisson()) 
 def go_to_airport(env, passenger, airport):
     # Passenger arrives at the airport
     arrival_time = env.now
-
     with airport.security.request() as request:
         yield request
-        yield env.process(airport.check_in(passenger))
-
-  
+        yield env.process(airport.check_in(passenger))  
     if random.choice([True, False]):
         with airport.server.request() as request:
             yield request
-           
-
-    # passenger heads into the airport
+    # passenger go to the airport
     wait_times.append(env.now - arrival_time)
-
-
 def run_airport(env, num_securities, num_servers):
     airport = Airport(env, num_securities, num_servers)
-
     for passenger in range(3):
         env.process(go_to_airport(env, passenger, airport))
-
     while True:
         yield env.timeout(0.20)  # Wait before generating a new person, using mean interarival rate
-
         passenger += 1
         env.process(go_to_airport(env, passenger, airport))
-
-
 def get_average_wait_time(wait_times):
     average_wait = statistics.mean(wait_times)
     # Print the results
     minutes, frac_minutes = divmod(average_wait, 1)
     seconds = frac_minutes * 60
     return round(minutes), round(seconds)
-
-
 def get_user_input():
     num_securities = input("Input # of security personnel working: ")
     num_servers = input("Input # of servers working: ")   
@@ -67,25 +51,16 @@ def get_user_input():
         )
         params = [1, 1, 1]
     return params
-
-
-def main():
-    # Setup
+def main():   
     random.seed(42)
-    num_securities, num_servers = get_user_input()
-
-    # Run the simulation
+    num_securities, num_servers = get_user_input()   
     env = simpy.Environment()
     env.process(run_airport(env, num_securities, num_servers))
-    env.run(until=5)
-
-    # View the results
+    env.run(until=5)    
     mins, secs = get_average_wait_time(wait_times)
     print(
         "Running simulation...",
         f"\nThe average wait time is {mins} minutes and {secs} seconds.",
     )
-
-
 if __name__ == "__main__":
     main()
