ac2a4e5261ded3e1e32769f561d50e4162d020df
LeenaKH123 <engleena@yahoo.com>
2023-03-15 20:14:35 +0300

    test

--- simulation/frf2.r
+++ simulation/frf2.r
@@ -1,3 +1,9 @@
 # install.packages("FrF2")
 library(FrF2)
-
+set.seed(40)
+# 16 houses and 10 features
+houses_algo <- FrF2(16, 10, factor.names = c(
+    'Large Yard', 'Pool', 'Solar Roof', 'Long Driveway', 'Multi Car Garage',
+    'Walk-In Closet', 'Man Cave', 'Full Bar', 'Gazebo', 'Elevator'
+    ), default.levels = c("1", "-1"))
+print(houses_algo)
