d51861a44390cc55e4ae35885c7f7eb786ea8217
LeenaKH123 <engleena@yahoo.com>
2023-03-29 19:48:44 +0300

    impuation in ptogress

--- imputation/imputation.r
+++ imputation/imputation.r
@@ -1,3 +1,19 @@
 rm(list = ls())
 ImputationData <- read.table('/home/lilo/rcode/imputation/imputationData.txt', stringsAsFactors = FALSE, header = FALSE, sep = ",")
 head(ImputationData)
+summary(ImputationData)
+# find the missing data
+impute_me <- which(ImputationData$V7 == "?")
+impute_me
+
+# how much of our data is missing
+length(impute_me)/nrow(ImputationData)
+# since we want to evenually use this data to classify v11, it is important to check out if the missing data is biased in some way
+bcw_clean <- ImputationData[-impute_me,]
+bcw_clean
+bcw_missing <- ImputationData[impute_me, ]
+bcw_missing
+table(ImputationData$V11)
+table(bcw_clean$V11)
+table(bcw_missing$V11)
+
